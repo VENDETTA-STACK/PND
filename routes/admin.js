e90ff82aab6fd4c58434b9d1b302946f7fad7a5d
ItFuturzTech9Teen <jagannathtech9teen@gmail.com>
2020-05-15 08:45:58 -0700

    asd

--- routes/admin.js
+++ routes/admin.js
@@ -9,14 +9,12 @@ var router = express.Router();
 var cors = require('cors');
 
 /* Data Models */
-var adminSchema = require('../data_models/admin-create');
+var adminSchema = require('../data_models/a-signup');
+var settingsSchema = require('../data_models/o-settings');
+var orderSchema = require('../data_models/new-order');
+var courierSchema = require('../data_models/courier-signup');
 
-/* Routes. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Invalid URL' });
-});
-
-router.post('/createAdmin',cors(),async function(req,res,next){
+router.post('/signup',cors(),async function(req,res,next){
     const {name,username,password,type} = req.body;
     try{
         var existAdmin = await adminSchema.find({username:username.toLowerCase()});
@@ -41,31 +39,137 @@ router.post('/createAdmin',cors(),async function(req,res,next){
     }
 });
 
-router.post('/adminLogin',cors(),async function(req,res,next){
+router.post('/login',cors(),async function(req,res,next){
     const {username,password,type} = req.body;
-    var existAdmin = await adminSchema.find(
-        {   username:username.toLowerCase(),
-            password:password.toLowerCase(),
-            type:type.toLowerCase()});
-    if(existAdmin.length!=0){
-        res.status(200)
-        .json({Message:"user found!",Data:existAdmin,IsSuccess:true});
-    }else{
-        res.status(200)
-        .json({Message:"user not found!",Data:existAdmin,IsSuccess:true});
+    try{
+        var existAdmin = await adminSchema.find(
+            {   username:username.toLowerCase(),
+                password:password.toLowerCase(),
+                type:type.toLowerCase()});
+        if(existAdmin.length!=0){
+            res.status(200)
+            .json({Message:"user found!",Data:existAdmin,IsSuccess:true});
+        }else{
+            res.status(200)
+            .json({Message:"user not found!",Data:existAdmin,IsSuccess:true});
+        }
+    }catch(err){
+        res.status(500)
+        .json({Message:err.message,Data:0,IsSuccess:false});
     }
 });
 
-router.post('/validateAdmin',cors(),async function(req,res,next){
+router.post('/validate',cors(),async function(req,res,next){
     const {id,type} = req.body;
-    var existAdmin = await adminSchema.find({'_id':id,type:type});
-    if(existAdmin.length!=0){
-        res.status(200)
-        .json({Message:"user found!",Data:existAdmin,IsSuccess:true});
-    }else{
-        res.status(200)
-        .json({Message:"user not found!",Data:existAdmin,IsSuccess:true});
+    try{
+        var existAdmin = await adminSchema.find({'_id':id,type:type});
+        if(existAdmin.length!=0){
+            res.status(200)
+            .json({Message:"user found!",Data:existAdmin,IsSuccess:true});
+        }else{
+            res.status(200)
+            .json({Message:"user not found!",Data:existAdmin,IsSuccess:true});
+        }
+    }catch(err){
+        res.status(500)
+        .json({Message:err.message,Data:0,IsSuccess:false});
     }
 });
 
+router.post('/updatesetttings',cors(),async function(req,res,next){
+    const {PerUnder5KM,PerKM,ExpDelivery} = req.body;
+    try{
+        var existData = await settingsSchema.find({});
+        if(existData.length == 1){
+            let id = existData[0].id;
+            let updatedsettings = {PerUnder5KM:PerUnder5KM,PerKM:PerKM,ExpDelivery:ExpDelivery};
+            await settingsSchema.findByIdAndUpdate(id,updatedsettings);
+            res.status(200)
+            .json({Message:"Settings Updated!",Data:1,IsSuccess:true});
+        }else{
+            var newsettings = new settingsSchema({PerUnder5KM:PerUnder5KM,PerKM:PerKM,ExpDelivery:ExpDelivery});
+            await newsettings.save();
+            res.status(200)
+            .json({Message:"Settings Created!",Data:1,IsSuccess:true});
+        }
+    }catch(err){
+        res.status(500)
+        .json({Message:err.message,Data:0,IsSuccess:false});
+    }
+});
+
+router.post('/settings',cors(),async function(req,res,next){
+    try{
+      var getsettings = await settingsSchema.find({});
+      if(getsettings.length == 1){
+        res.status(200)
+        .json({Message:"Settings Found!",Data:getsettings,IsSuccess:true});
+      }else{
+        res.status(200)
+        .json({Message:"Settings Not Found!",Data:getsettings,IsSuccess:true});
+      }
+    }catch(err){
+      res.status(500)
+      .json({Message:err.message,Data:0,IsSuccess:false});
+    }
+});
+
+router.post('/orders',cors(),async function(req,res,next){
+    try{
+      orderSchema.find({})
+      .populate('courierId','firstName lastName fcmToken mobileNo accStatus transport isVerified')
+      .populate('customerId')
+      .exec()
+      .then(docs=>{
+        if(docs.length!=0){
+          res.status(200)
+          .json({Message:"Order Found!",Data:docs,IsSuccess:true});
+        }else{
+          res.status(200)
+          .json({Message:"No Order Found!",Data:docs,IsSuccess:true});
+        }
+      });
+    }catch(err){
+      res.status(500)
+      .json({Message:err.message,Data:0,IsSuccess:false});
+    }
+});
+
+router.post('/couriers',cors(),async function(req,res,next){
+    try{
+       courierSchema.find({})
+      .exec()
+      .then(docs=>{
+        if(docs.length!=0){
+          res.status(200)
+          .json({Message:"Courier Found!",Data:docs,IsSuccess:true});
+        }else{
+          res.status(200)
+          .json({Message:"No Courier Found!",Data:docs,IsSuccess:true});
+        }
+      });
+    }catch(err){
+      res.status(500)
+      .json({Message:err.message,Data:0,IsSuccess:false});
+    }
+});
+
+router.post('/users',cors(),async function(req,res,next){
+    try{
+       adminSchema.find({})
+      .exec()
+      .then(docs=>{
+        if(docs.length!=0){
+          res.status(200)
+          .json({Message:"Users Found!",Data:docs,IsSuccess:true});
+        }else{
+          res.status(200)
+          .json({Message:"No Users Found!",Data:docs,IsSuccess:true});
+        }
+      });
+    }catch(err){
+      res.status(500)
+      .json({Message:err.message,Data:0,IsSuccess:false});
+    }
+});
 module.exports = router;
