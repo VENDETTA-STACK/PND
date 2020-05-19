cfbc93d490ff4e4c091b8e6e0d49b73591348afe
ItFuturzTech9Teen <jagannathtech9teen@gmail.com>
2020-05-19 01:43:20 -0700

    asd

--- data_models/o-settings.js
+++ data_models/o-settings.js
@@ -4,7 +4,8 @@ const orderSettingsSchema = mongoose.Schema({
     _id:mongoose.Schema.Types.ObjectId,
     PerUnder5KM:{type:Number,required:true},
     PerKM:{type:Number,required:true},
-    ExpDelivery:{type:Number,required:true}
+    ExpDelivery:{type:Number,required:true},
+    ReferalPoint:{type:Number,required:true}
 });
 
 module.exports = mongoose.model('Settings',orderSettingsSchema);
