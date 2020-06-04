4dbfad00a5dbf5445647e1f0d3edb540490c4e49
ItFuturzTech9Teen <jagannathtech9teen@gmail.com>
2020-06-04 15:43:26 +0530

    dfasd

--- data_models/order.model.js
+++ data_models/order.model.js
@@ -1,128 +1,126 @@
 const mongoose = require("mongoose");
 
 const orderSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId,
-  orderNo: { type: String, required: true },
-  customerId: { type: mongoose.Types.ObjectId, ref: "Customers" },
-  deliveryType: {
-    type: String,
-    required: true,
-  },
-  weightLimit: {
-    type: String,
-    required: true,
-  },
-  pickupPoint: {
-    name: {
-      type: String,
-      required: true,
+    _id: mongoose.Schema.Types.ObjectId,
+    orderNo: { type: String, required: true },
+    customerId: { type: mongoose.Types.ObjectId, ref: "Customers" },
+    deliveryType: {
+        type: String,
+        required: true,
     },
-    mobileNo: {
-      type: String,
-      required: true,
+    weightLimit: {
+        type: String,
+        required: true,
     },
-    address: {
-      type: String,
-      required: true,
+    pickupPoint: {
+        name: {
+            type: String,
+            required: true,
+        },
+        mobileNo: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        lat: {
+            type: String,
+            required: true,
+        },
+        long: {
+            type: String,
+            required: true,
+        },
+        completeAddress: {
+            type: String,
+            required: true,
+        },
+        contents: {
+            type: String,
+            required: true,
+        },
+        arriveType: {
+            type: String,
+        },
+        arriveTime: {
+            type: String,
+        },
     },
-    lat: {
-      type: String,
-      required: true,
+    deliveryPoint: {
+        name: {
+            type: String,
+            required: true,
+        },
+        mobileNo: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        lat: {
+            type: String,
+            required: true,
+        },
+        long: {
+            type: String,
+            required: true,
+        },
+        completeAddress: {
+            type: String,
+            required: true,
+        },
+        distance: {
+            type: Number,
+            required: true,
+        },
     },
-    long: {
-      type: String,
-      required: true,
+    collectCash: {
+        type: String,
+        required: true,
     },
-    completeAddress: {
-      type: String,
-      required: true,
+    promoCode: {
+        type: String,
+        required: true,
     },
-    contents: {
-      type: String,
-      required: true,
+    amount: {
+        type: Number,
+        required: true,
     },
-    arriveType: {
-      type: String,
-      required: true,
+    discount: {
+        type: Number,
+        required: true,
     },
-    arriveTime: {
-      type: String,
-      required: true,
+    additionalAmount: {
+        type: Number,
+        required: true,
     },
-  },
-  deliveryPoint: {
-    name: {
-      type: String,
-      required: true,
+    finalAmount: {
+        type: Number,
+        required: true,
     },
-    mobileNo: {
-      type: String,
-      required: true,
+    status: {
+        type: String,
+        required: true,
     },
-    address: {
-      type: String,
-      required: true,
+    note: {
+        type: String,
+        required: true,
     },
-    lat: {
-      type: String,
-      required: true,
+    isActive: {
+        type: Boolean,
+        default: true,
     },
-    long: {
-      type: String,
-      required: true,
+    dateTime: {
+        type: Date,
+        default: Date.now,
     },
-    completeAddress: {
-      type: String,
-      required: true,
-    },
-    distance: {
-      type: Number,
-      required: true,
-    },
-  },
-  collectCash: {
-    type: String,
-    required: true,
-  },
-  promoCode: {
-    type: String,
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-  },
-  discount: {
-    type: Number,
-    required: true,
-  },
-  additionalAmount: {
-    type: Number,
-    required: true,
-  },
-  finalAmount: {
-    type: Number,
-    required: true,
-  },
-  status: {
-    type: String,
-    required: true,
-  },
-  note: {
-    type: String,
-    required: true,
-  },
-  isActive: {
-    type: Boolean,
-    default: true,
-  },
-  dateTime: {
-    type: Date,
-    default: Date.now,
-  },
-  courierId: [
-    { type: mongoose.Types.ObjectId, ref: "Couriers", default: null },
-  ],
+    courierId: [
+        { type: mongoose.Types.ObjectId, ref: "Couriers", default: null },
+    ],
 });
 
 module.exports = mongoose.model("Orders", orderSchema);
