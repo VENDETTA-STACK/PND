62a738d71a006d9a8f6b23f29a7ecc5dc20a30a2
ItFuturzTech9Teen <jagannathtech9teen@gmail.com>
2020-06-10 16:23:05 +0530

    dfg

--- routes/courier.js
+++ routes/courier.js
@@ -27,9 +27,11 @@ var fieldset = finalstorage.fields([
     { name: "poaBackImg", maxCount: 1 },
     { name: "panCardImg", maxCount: 1 },
     { name: "electricityImg", maxCount: 1 },
-    { name: "policeVerificationImg", maxCount: 1 },
 ]);
 
+
+// { name: "policeVerificationImg", maxCount: 1 },
+// req.files.policeVerificationImg[0].path
 /* Data Models */
 var courierSchema = require("../data_models/courier.signup.model");
 var courierNotificationSchema = require("../data_models/courier.notification.model");
@@ -70,10 +72,10 @@ router.post("/signup", fieldset, async function(req, res, next) {
                 fs.unlinkSync(old.replace("\\g", "/"));
             }
 
-            old = req.files.policeVerificationImg[0].path;
-            if (fs.existsSync(old.replace("\\g", "/"))) {
-                fs.unlinkSync(old.replace("\\g", "/"));
-            }
+            // old = req.files.policeVerificationImg[0].path;
+            // if (fs.existsSync(old.replace("\\g", "/"))) {
+            //     fs.unlinkSync(old.replace("\\g", "/"));
+            // }
 
             res.status(200).json({
                 Message: "Courier Already Registered!",
@@ -95,7 +97,7 @@ router.post("/signup", fieldset, async function(req, res, next) {
                 poaBackImg: req.files.poaBackImg[0].path,
                 panCardImg: req.files.panCardImg[0].path,
                 electricityImg: req.files.electricityImg[0].path,
-                policeVerificationImg: req.files.policeVerificationImg[0].path,
+                policeVerificationImg: "",
             });
             await newCourier.save();
             res
