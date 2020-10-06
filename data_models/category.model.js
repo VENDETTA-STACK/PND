28f9697bc37bb099862e124b0a62d54bd2005cb3
dhanpalpixelbeget <dhanpal@pixelbeget.com>
2020-10-06 14:38:52 +0530

    updated 06 10

---
+++ data_models/category.model.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const categorySchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    title: {
+        type: String,
+    },
+    image: {
+        type: String,
+    },
+    price: {
+        type: Number,
+    },
+    note: {
+        type: String
+    },
+    dateTime: {
+        type: Date,
+        default: Date.now,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+});
+
+module.exports = mongoose.model("parcelcategories", categorySchema);
---
+++ data_models/settings.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const orderSettingsSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  PerUnder5KM: { type: Number, required: true },
+  PerKM: { type: Number, required: true },
+  ReferalPoint: { type: Number, required: true },
+  AppLink: { type: String},
+  WhatsAppNo: { type: String},
+  DefaultWMessage: { type: String},
+  AmountPayKM: { type: Number,},
+  TermsnConditionURL: { type: String,},
+  
+});
+
+module.exports = mongoose.model("Settings", orderSettingsSchema);
